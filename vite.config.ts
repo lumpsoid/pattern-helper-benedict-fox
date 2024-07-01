5e3c16cd2c2564f17cb420bc67aed13292308569
lumpsoid <mmgeeki@gmail.com>
2024-07-02 01:45:13 +0200

    build: fixed github pages publishing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/pattern-helper-benedict-fox/',
 	plugins: [
 		preact({
 			prerender: {
